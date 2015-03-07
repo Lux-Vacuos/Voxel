fdd835bac40e9da0a4030b4ce462cc378c1286cf
Guerra24 <pablo230699@hotmail.com>
2015-03-06 18:28:33 -0600

    Updated Lib

--- src/net/voxel/core/util/Font.java
+++ src/net/voxel/core/util/Font.java
@@ -2,4 +2,13 @@ package net.voxel.core.util;
 
 public class Font {
 	
+	private Texture fontTexture;
+	
+	public Texture loadFont(String location) {
+		fontTexture = Texture.loadTexture(location);
+		return fontTexture;
+	}
+	public void bind() {
+		fontTexture.bind();
+	}
 }
