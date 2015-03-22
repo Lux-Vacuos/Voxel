a38868501d13ccde5d3da245a08845ef79b088c3
Guerra24 <pablo230699@hotmail.com>
2015-03-21 23:52:10 -0600

    Now can work in jar. Add natives in a folder called "dlls"

--- src/net/voxel/Main.java
+++ src/net/voxel/Main.java
@@ -94,6 +94,7 @@ public class Main extends Screen{
 	}
 	
 	public static void main(String[] args){
+		System.setProperty("org.lwjgl.librarypath",System.getProperty("user.dir") + "\\dlls");
 		Window.createWindow(Constants.WIDTH, Constants.HEIGHT, "Voxels", true);
 		new Main();
 	}
