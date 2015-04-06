bedf18a14140b9400ba99eadf6c116acbd1901bd
Guerra24 <pablo230699@hotmail.com>
2015-04-06 16:50:05 -0600

    Fix

--- assets/shaders/fragmentShaderEntity.glsl
+++ assets/shaders/fragmentShaderEntity.glsl
@@ -19,7 +19,7 @@ void main(void) {
 	vec3 unitLightVector = normalize(toLightVector);
 	
 	float nDotl = dot(unitNormal,unitLightVector);
-	float brightness = max(nDotl, 0.0);
+	float brightness = max(nDotl, 0.1);
 	vec3 diffuse = brightness * lightColour;
 		
 	vec4 textureColour = texture(textureSampler, pass_textureCoords);
