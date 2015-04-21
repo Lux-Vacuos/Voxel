f149ace4efcd6880320daf808ef76f96b85bc3c4
Guerra24 <pablo230699@hotmail.com>
2015-04-20 18:43:04 -0600

    Organization and testing

--- srcClient/net/guerra24/voxel/client/engine/world/chunks/Chunk.java
+++ srcClient/net/guerra24/voxel/client/engine/world/chunks/Chunk.java
@@ -16,40 +16,50 @@ public class Chunk {
 	private static int[][][] blocks;
 
 	public static void create(int chunkX, int chunkZ) {
-		float xOffset = CHUNK_SIZE * chunkX - 303;
-		float zOffset = CHUNK_SIZE * chunkZ - 303;
+		int xOffset = CHUNK_SIZE * chunkX - 303;
+		int zOffset = CHUNK_SIZE * chunkZ - 303;
 
 		blocks = new int[CHUNK_SIZE][CHUNK_HEIGHT][CHUNK_SIZE];
 		for (int x = 0; x < CHUNK_SIZE; x++) {
 			for (int y = 0; y < CHUNK_HEIGHT; y++) {
 				for (int z = 0; z < CHUNK_SIZE; z++) {
-
-					int x2 = (int) (SimplexNoise.noise(x, y) * CHUNK_SIZE);
-					int z2 = (int) (SimplexNoise.noise(z, y) * CHUNK_SIZE);
-					if (Engine.rand.nextInt(9) == 0) {
-						blocks[x][y][z] = BlocksType.Stone.getId();
-					} else if (Engine.rand.nextInt(9) == 0) {
-						blocks[x][y][z] = BlocksType.Grass.getId();
-					}
-					if (blocks[x][y][z] == 1) {
-						Engine.allCubes.add(new Entity(Blocks.cubeStone,
-								new Vector3f(x2 + xOffset, y, z2 + zOffset),
-								0f, 0f, 0f, 1f));
-					} else if (blocks[x][y][z] == 2) {
-						Engine.allCubes.add(new Entity(Blocks.cubeGrass,
-								new Vector3f(x2 + xOffset, y, z2 + zOffset),
-								0f, 0f, 0f, 1f));
-					}
-
-					if (y == 0) {
-						if (Engine.rand.nextInt(1) == 0) {
-							Engine.allCubes.add(new Entity(Blocks.cubeIndes,
-									new Vector3f(x + xOffset, y, z + zOffset),
-									0, 0, 0, 1));
-						}
-					}
-
+					int x2 = (int) (SimplexNoise.noise(x, z) * CHUNK_SIZE);
+					int z2 = (int) (SimplexNoise.noise(z, x) * CHUNK_SIZE);
+					create(x, y, z);
+					render(x, y, z, x2, z2, xOffset, zOffset);
 				}
+			}
+		}
+	}
+
+	private static void create(int x, int y, int z) {
+		if (Engine.rand.nextInt(2) == 0) {
+			blocks[x][y][z] = BlocksType.Stone.getId();
+		} else if (Engine.rand.nextInt(2) == 0) {
+			blocks[x][y][z] = BlocksType.Grass.getId();
+		}
+	}
+
+	private static void render(int x, int y, int z, int x2, int z2,
+			int xOffset, int zOffset) {
+		if (y <= 64 && y >= 60) {
+			if (blocks[x][y][z] == 1) {
+				Engine.allCubes.add(new Entity(Blocks.cubeGrass, new Vector3f(
+						x2 + xOffset, y, z2 + zOffset), 0f, 0f, 0f, 1f));
+
+			}
+		}
+		if (y <= 59) {
+			if (blocks[x][y][z] == 2) {
+				Engine.allCubes.add(new Entity(Blocks.cubeStone, new Vector3f(
+						x2 + xOffset, y, z2 + zOffset), 0f, 0f, 0f, 1f));
+			}
+		}
+
+		if (y == 0) {
+			if (Engine.rand.nextInt(1) == 0) {
+				Engine.allCubes.add(new Entity(Blocks.cubeIndes, new Vector3f(x
+						+ xOffset, y, z + zOffset), 0, 0, 0, 1));
 			}
 		}
 	}
