8423f95ed125dfdbc3f690913b5a0a4dcca339d3
Guerra24 <pablo230699@hotmail.com>
2016-03-19 17:03:41 -0600

    Build-181: Fixed Bug
    # Changes
    Removed unused OpenAL Compat Layer files.
    <gradlew> file can now be run with ./gradlew
    
    # BugFixes
    Fixed OpenAL with latest LWJGL3 version.

--- src/main/java/net/luxvacuos/voxel/client/sound/openal/AL.java
+++ src/main/java/net/luxvacuos/voxel/client/sound/openal/AL.java
@@ -5,8 +5,13 @@ import static org.lwjgl.openal.ALC10.ALC_FREQUENCY;
 import static org.lwjgl.openal.ALC10.ALC_REFRESH;
 import static org.lwjgl.openal.ALC10.ALC_SYNC;
 import static org.lwjgl.openal.ALC10.ALC_TRUE;
+import static org.lwjgl.openal.ALC10.alcCloseDevice;
+import static org.lwjgl.openal.ALC10.alcCreateContext;
+import static org.lwjgl.openal.ALC10.alcDestroyContext;
 import static org.lwjgl.openal.ALC10.alcGetInteger;
 import static org.lwjgl.openal.ALC10.alcGetString;
+import static org.lwjgl.openal.ALC10.alcMakeContextCurrent;
+import static org.lwjgl.openal.ALC10.alcOpenDevice;
 import static org.lwjgl.openal.ALC11.ALC_ALL_DEVICES_SPECIFIER;
 import static org.lwjgl.openal.ALC11.ALC_MONO_SOURCES;
 import static org.lwjgl.openal.ALC11.ALC_STEREO_SOURCES;
@@ -15,59 +20,54 @@ import static org.lwjgl.system.MemoryUtil.NULL;
 import java.nio.ByteBuffer;
 import java.util.List;
 
+import org.lwjgl.openal.ALC;
 import org.lwjgl.openal.ALCCapabilities;
-import org.lwjgl.openal.ALContext;
-import org.lwjgl.openal.ALDevice;
 import org.lwjgl.openal.ALUtil;
 
 import net.luxvacuos.voxel.client.util.Logger;
 
 public class AL {
 
-	static ALContext alContext;
-	static ALCdevice alcDevice;
-	static ALDevice alDevice;
+	static long device;
+	static long context;
 
 	private static boolean created = false;
 
 	public static void create() {
-		if (alContext == null) {
+		device = alcOpenDevice((ByteBuffer) null);
+		if (device == NULL)
+			throw new IllegalStateException("Failed to open the default device.");
 
-			alDevice = ALDevice.create(null);
-			if (alDevice == null)
-				throw new IllegalStateException("Failed to open the default device.");
+		ALCCapabilities deviceCaps = ALC.createCapabilities(device);
 
-			ALCCapabilities caps = alDevice.getCapabilities();
+		Logger.log("OpenALC10: " + deviceCaps.OpenALC10);
+		Logger.log("OpenALC11: " + deviceCaps.OpenALC11);
+		Logger.log("caps.ALC_EXT_EFX = " + deviceCaps.ALC_EXT_EFX);
 
-			Logger.log("OpenALC10: " + caps.OpenALC10);
-			Logger.log("OpenALC11: " + caps.OpenALC11);
-			Logger.log("ALC_EXT_EFX: " + caps.ALC_EXT_EFX);
-
-			if (caps.OpenALC11) {
-				List<String> devices = ALUtil.getStringList(NULL, ALC_ALL_DEVICES_SPECIFIER);
-				if (devices == null)
-					ALUtil.checkALCError(NULL);
-				else {
-					for (int i = 0; i < devices.size(); i++)
-						Logger.log("Device " + i + ": " + devices.get(i));
-				}
+		if (deviceCaps.OpenALC11) {
+			List<String> devices = ALUtil.getStringList(NULL, ALC_ALL_DEVICES_SPECIFIER);
+			if (devices == null)
+				ALUtil.checkALCError(NULL);
+			else {
+				for (int i = 0; i < devices.size(); i++)
+					Logger.log(i + ": " + devices.get(i));
 			}
-
-			String defaultDeviceSpecifier = alcGetString(0L, ALC_DEFAULT_DEVICE_SPECIFIER);
-			Logger.log("Default device: " + defaultDeviceSpecifier);
-
-			long contextHandle = org.lwjgl.openal.ALC10.alcCreateContext(alDevice.address(), (ByteBuffer) null);
-			alContext = ALContext.create(alDevice);
-			alcDevice = new ALCdevice(contextHandle);
-
-			Logger.log("ALC_FREQUENCY: " + alcGetInteger(alDevice.address(), ALC_FREQUENCY) + "Hz");
-			Logger.log("ALC_REFRESH: " + alcGetInteger(alDevice.address(), ALC_REFRESH) + "Hz");
-			Logger.log("ALC_SYNC: " + (alcGetInteger(alDevice.address(), ALC_SYNC) == ALC_TRUE));
-			Logger.log("ALC_MONO_SOURCES: " + alcGetInteger(alDevice.address(), ALC_MONO_SOURCES));
-			Logger.log("ALC_STEREO_SOURCES: " + alcGetInteger(alDevice.address(), ALC_STEREO_SOURCES));
-
-			created = true;
 		}
+
+		String defaultDeviceSpecifier = alcGetString(NULL, ALC_DEFAULT_DEVICE_SPECIFIER);
+		Logger.log("Default device: " + defaultDeviceSpecifier);
+
+		context = alcCreateContext(device, (ByteBuffer) null);
+		alcMakeContextCurrent(context);
+		org.lwjgl.openal.AL.createCapabilities(deviceCaps);
+
+		Logger.log("ALC_FREQUENCY: " + alcGetInteger(device, ALC_FREQUENCY) + "Hz");
+		Logger.log("ALC_REFRESH: " + alcGetInteger(device, ALC_REFRESH) + "Hz");
+		Logger.log("ALC_SYNC: " + (alcGetInteger(device, ALC_SYNC) == ALC_TRUE));
+		Logger.log("ALC_MONO_SOURCES: " + alcGetInteger(device, ALC_MONO_SOURCES));
+		Logger.log("ALC_STEREO_SOURCES: " + alcGetInteger(device, ALC_STEREO_SOURCES));
+
+		created = true;
 	}
 
 	public static boolean isCreated() {
@@ -75,15 +75,9 @@ public class AL {
 	}
 
 	public static void destroy() {
-		alContext.destroy();
-		alDevice.close();
-		alContext = null;
-		alcDevice = null;
+		alcDestroyContext(context);
+		alcCloseDevice(device);
 		created = false;
-	}
-
-	public static ALCdevice getDevice() {
-		return alcDevice;
 	}
 
 }
